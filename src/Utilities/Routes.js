17afefcb46dfdda0558db15c490c676847cc5647
tuhin180 <tanjirtuhin18@gmail.com>
2022-10-11 04:55:30 +0600

    all router setup is done

--- src/Utilities/Routes.js
+++ src/Utilities/Routes.js
@@ -2,13 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Home from "../components/Home/Home";
 import Main from "../Layouts/Main";
+import Quiz from "../components/Quiz/Quiz";
+import Chart from "../components/Chart/Chart";
+import Blog from "../components/Blog/Blog";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
     errorElement: <ErrorPage></ErrorPage>,
-    children: [{ path: "/", element: <Home></Home> }],
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/quiz", element: <Quiz></Quiz> },
+      { path: "/chart", element: <Chart></Chart> },
+      { path: "/blog", element: <Blog></Blog> },
+    ],
   },
 ]);
 export default router;
